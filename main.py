84d59f5c1710acf1eba77109c533d310369c22a2
bobcermak <bohuslavcermak62@gmail.com>
2023-12-23 22:04:44 +0100

    hotovo!!!

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import winsound
 import time
 
-cas = (input("Napiš čas, který potřebuješ nastavit, vše udávej v sekundách: "))
-konecCas = int(cas) + 1
+while True:
+    cas = (input("Napiš čas, který potřebuješ nastavit, vše udávej v sekundách: "))
+    try:
         
-for i in range(1,konecCas):
-    time.sleep(1)
-    print(float(i))
-                            
-if konecCas:
-    winsound.Beep(200, 5000)
+        for i in range(int(cas),-1,-1):
+            if i < int(cas):
+                time.sleep(1.00)
+                print(float(i), end= '\r')
+            else:
+                print(float(i), end= '\r')                            
+        
+        winsound.Beep(200, 3000)
+        
+            
+    except ValueError:
+        time.sleep(1)
+        print("Zadal jste neplatný formát")
+        time.sleep(1)
+
 
 
             
         
-        
+    
 
     
     
